0ed53e8058ea1caeee8206b32963b062cc758871
Fatma Hameed <fatma78hameed@gmail.com>
2020-09-21 03:28:55 -0700

    finished exercise 3 w3 js3 (trivia app)

---
+++ Week3/homework/js-exercises/trivia-app/trivia.js
@@ -0,0 +1,54 @@
+function main() {
+  // Create DOM elements
+  const container = document.createElement('div');
+  const header = document.createElement('h1');
+  const infoParagraph = document.createElement('p');
+  const ulElement = document.createElement('ul');
+  header.textContent = "Let's play some Trivia!";
+  infoParagraph.textContent =
+    'Try your best to figure out the answer. If you relly have no clue, click on the question to reveal the answer...';
+  document.body.appendChild(container);
+  container.appendChild(header);
+  container.appendChild(infoParagraph);
+  container.appendChild(ulElement);
+
+  // Create a function to get and manipulate the data
+
+  async function getQuestions() {
+    const response = await fetch('https://opentdb.com/api.php?amount=5');
+    const data = await response.json();
+    let questionsLiEl;
+    data.results.forEach(result => {
+      const { question } = result;
+
+      const correctAnswer = result.correct_answer;
+      // Decode special characters appeared in the data that fetched(using jquery)
+      const parsedQuestion = $('<textarea />')
+        .html(question)
+        .text();
+      const parsedCorrectAnswer = $('<textarea />')
+        .html(correctAnswer)
+        .text();
+
+      // create li elements and assign their text content to the parsed questions, then append them to ul element
+      questionsLiEl = document.createElement('li');
+      questionsLiEl.textContent = parsedQuestion;
+      ulElement.appendChild(questionsLiEl);
+
+      // create div elements and assign their text content to the parsed answers, then append them to ul element
+      const answer = document.createElement('div');
+      ulElement.appendChild(answer);
+
+      // Create event listener for getting answers of the questions when they are clicked
+      questionsLiEl.addEventListener('click', event => {
+        if (event.target.textContent === parsedQuestion) {
+          answer.textContent = parsedCorrectAnswer;
+          answer.style.display = 'block';
+        }
+      });
+    });
+  }
+  getQuestions();
+}
+
+window.addEventListener('load', main);
